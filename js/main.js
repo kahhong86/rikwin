faa1b6efb501636966b1bb739ac778e8f4f85cc0
Cheong Kah Hong <kahhong_cheong@git.savantdegrees.com>
2026-02-24 10:30:16 +0800

    Mobile changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-  	
+    // Initialize Swiper
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1.2,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            type: "progressbar",
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            320: { slidesPerView: 1.1 },
+            480: { slidesPerView: 1.1 }
+        }
+    });
+
+    document.querySelector('.mobile-hamburger').addEventListener('click', function() {
+        document.querySelector('#main-navigation').classList.toggle('active');
+        document.querySelector('.mobile-hamburger').classList.toggle('active');
+    });
 });
